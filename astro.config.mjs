c78fd68b0a95df9e7a161164a3fd9ba2306080bb
Nicoka11 <pro@nicolaska.co>
2026-06-06 12:23:19 +0100

    chore: add switzer font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Switzer',
+      cssVariable: '--font-switzer',
+      options: {
+        variants: [
+          {
+            weight: '100 900',
+            style: 'normal',
+            src: ['./src/assets/fonts/switzer/Switzer-Variable.woff2'],
+          },
+          {
+            weight: '100 900',
+            style: 'italic',
+            src: ['./src/assets/fonts/switzer/Switzer-VariableItalic.woff2'],
+          },
+        ],
+      },
+    },
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
